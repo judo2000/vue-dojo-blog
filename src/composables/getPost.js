204329b1afc94112e1541bf16a2880cfe1b33285
Stephen Moore <judo2000@comcast.net>
2022-07-12 17:53:22 -0600

    Update getPost to get a single post from firebase

--- src/composables/getPost.js
+++ src/composables/getPost.js
@@ -1,4 +1,6 @@
 import { ref } from 'vue';
+import { projectFirestore } from '../firebase/config';
+import { doc, getDoc } from 'firebase/firestore';
 
 const getPost = (id) => {
   const post = ref(null);
@@ -10,13 +12,16 @@ const getPost = (id) => {
       //   setTimeout(resolve, 2000);
       // });
 
-      let data = await fetch('http://localhost:3000/posts/' + id);
-      if (!data.ok) {
+      const postRef = doc(projectFirestore, 'posts', id);
+      let postSnapshot = await getDoc(postRef);
+      console.log(postSnapshot);
+      if (!postSnapshot._document) {
         throw Error('That post does not exist');
       }
-      post.value = await data.json();
+      post.value = { ...postSnapshot.data(), id: postSnapshot.id };
     } catch (err) {
       error.value = err.message;
+      console.log(err);
     }
   };
 
